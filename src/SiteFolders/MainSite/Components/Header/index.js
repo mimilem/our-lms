47d0df7bf9f095f651975b612364ac963d07aae6
Nephthali Salam <snephthali@gmail.com>
2021-12-07 18:29:01 +0200

    Tab Navigation

--- src/SiteFolders/MainSite/Components/Header/index.js
+++ src/SiteFolders/MainSite/Components/Header/index.js
@@ -1,29 +1,32 @@
+/*
+    Here we organize and style the elements of the header 
+    and Navigation Tab.
+*/
+
 import React from 'react';
-import './header.css'
+import './header.css';
+
 
 function Header() {
-
     return (
         <header className='container'>
             <div className='logoContainer'>
                 <div className='logo'/>
             </div>
 
+            {/* Staff Login Form */}
             <div className='loginInputContainer'>
                 Staff login: 
                 <input
                     placeholder="Username"
-                    style={{width: 115, height:20, marginLeft:15, borderRadius: 10, borderWidth:0}}
                 />
                 <input
                     placeholder="Password"
-                    style={{width: 115, height:20, marginLeft:15, borderRadius: 10, borderWidth:0}}
                 />
-                <button style={{width: 115, height:25, marginLeft:15,borderWidth:1, backgroundColor:'#5D8AA8', color:'#fff'}}>
-                    Login
-                </button>
+                <button>Login</button>
             </div>
 
+            {/* Navigation Bar */}
             <div className="navContainer">
                 <ul className="tabElementsContainer">
                     <li className="tabElement">Home</li>
