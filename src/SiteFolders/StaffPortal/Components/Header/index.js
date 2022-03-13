f49084107a3ea04c311926a68d03c00c0b669eb0
Nephthali Salam <snephthali@gmail.com>
2022-03-13 15:16:18 +0200

    notifications

--- src/SiteFolders/StaffPortal/Components/Header/index.js
+++ src/SiteFolders/StaffPortal/Components/Header/index.js
@@ -6,6 +6,12 @@ import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { useDetectClickOutside } from 'react-detect-click-outside';
 
+// Import the amplify API and components to handle the 
+// requests.
+import { API, graphqlOperation } from "aws-amplify";
+import { listNotificationss } from '../../../../graphql/queries';
+import * as mutations from '../../../../graphql/mutations';
+
 //import the styling compnent(s).
 import './header.css';
 
@@ -13,12 +19,19 @@ import './header.css';
 function Header({ campusDetails }) {
     
     const [isShown, setIsShown] = useState(false);
+    const [isShownNotification, setIsShownNotification] = useState(false);
+    const [notifications, setNotifications] = useState([])
 
     const closeDropdown = () => {
         setIsShown(false);
     }
-
     const ref = useDetectClickOutside({ onTriggered: closeDropdown });
+
+    const closeNotifications = () => {
+        setIsShownNotification(false);
+    }
+    const refNotifications = useDetectClickOutside({ onTriggered: closeNotifications });
+
 
     // Initiate the window size.
     const [screenWidth, setScreenWidth] = useState(window.innerWidth)
@@ -32,6 +45,26 @@ function Header({ campusDetails }) {
         return () => {
             window.removeEventListener('resize', changeWidth)
         }
+    }, [])
+
+    /* fetch the list of all notifications */
+    useEffect( () => {
+        //automatically scroll to top
+        window.scrollTo(0,0);
+
+        const fetchNotifications = async () => {
+            try {
+                const notificationResults = await API.graphql(
+                    graphqlOperation(listNotificationss)
+                )
+                const notifications = notificationResults.data.listNotificationss.items
+                setNotifications(notifications)
+            } 
+            catch (error) {
+                console.log(error)
+            }
+        }
+        fetchNotifications();
     }, [])
 
     return (
@@ -60,27 +93,48 @@ function Header({ campusDetails }) {
                 )}
             </div> */}
 
-                <div onClick={() => setIsShown(true)} ref={ref} >
-
-                        <div className='user-settings'>
-                            <div className='user-avatar' />
+            <div
+                onClick={() => setIsShownNotification(true)} ref={refNotifications} 
+                className='notification-icon-container'>
+                <div className='notification-icon' />
+                {isShownNotification && (
+                    <ul className='notification-dropdown'>
+                        <div>
+                            {
+                                notifications.map(notification =>(
+                                    <div className='notification-list-container'>
+                                        <div className='notification-title'>{notification.title}</div>
+                                        <div className='notification-description'>{notification.description}</div>
+                                        <hr className='nav-tab-hr'/>
+                                    </div>
+                                ))
+                            }
                         </div>
-                        
-                        {isShown && (
-                            <ul className='profile-dropdown-menu'>
-                                <Link to='/Staff/AdminProfile' style={{textDecoration:'none'}}>
-                                    <li>
-                                        Profile
-                                    </li>
-                                </Link>
-                                <hr className='nav-tab-hr'/>
-                                <li>Settings</li>
-                                <hr className='nav-tab-hr'/>
-                                <Link to='/Staff-login' className='sign-out'>Sign out</Link>
-                            </ul> 
-                        )}
-                
+                    </ul> 
+                )}
+            </div>
+            
+            <div onClick={() => setIsShown(true)} ref={ref} >
+
+                <div className='user-settings'>
+                    <div className='user-avatar' />
                 </div>
+                
+                {isShown && (
+                    <ul className='profile-dropdown-menu'>
+                        <Link to='/Staff/AdminProfile' style={{textDecoration:'none'}}>
+                            <li>
+                                Profile
+                            </li>
+                        </Link>
+                        <hr className='nav-tab-hr'/>
+                        <li>Settings</li>
+                        <hr className='nav-tab-hr'/>
+                        <Link to='/Staff-login' className='sign-out'>Sign out</Link>
+                    </ul> 
+                )}
+            
+            </div>
         </div>
     );
 }
